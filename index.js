e6a4a67b3581b3477657cbc698560e5bfaea3953
Martha659 <martha.blackcloud@students.msswarriors.org>
2026-01-29 13:26:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ function markAsDone(todos) {
 todos[count] = "done - " + todos[count];
 count++;
     if(count < todos.length){
-    return MarkasDone(todos);
+    return markasDone(todos);
     }else{
         return todos;
     }
 }
+
 
 
